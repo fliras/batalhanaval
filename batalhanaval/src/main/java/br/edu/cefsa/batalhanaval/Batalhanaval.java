f5fd92d93febc48c6e507762e0eb172257e9c838
Felipe Lira <filipi6219@gmail.com>
2025-08-28 21:19:08 -0300

    chore: add projeto java

---
+++ batalhanaval/src/main/java/br/edu/cefsa/batalhanaval/Batalhanaval.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.edu.cefsa.batalhanaval;
+
+/**
+ *
+ * @author Felipe
+ */
+public class Batalhanaval {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

92e2629debbef3e77b656c54a63d543b9312cc08
Felipe Lira <filipi6219@gmail.com>
2025-09-07 22:34:15 -0300

    feat: implementa regra para checar o vencedor do jogo

---
+++ batalhanaval/src/main/java/br/edu/cefsa/batalhanaval/ChecagemDeVencedor.java
@@ -0,0 +1,20 @@
+package br.edu.cefsa.batalhanaval;
+
+public class ChecagemDeVencedor {
+    public Jogador checa(Jogador jogador1, Jogador jogador2) {
+        boolean jogador1EstaNoJogo = validaSeHaEmbarcacaoOperante(jogador1.obtemEmbarcacoes());
+        boolean jogador2EstaNoJogo = validaSeHaEmbarcacaoOperante(jogador2.obtemEmbarcacoes());
+        if (jogador1EstaNoJogo && jogador2EstaNoJogo)
+            return null; // não há vencedores no momento
+        if (jogador1EstaNoJogo)
+            return jogador1;
+        return jogador2;
+    }
+    
+    private boolean validaSeHaEmbarcacaoOperante(Embarcacao[] embarcacoes) {
+        for (Embarcacao embarcacao : embarcacoes)
+            if (embarcacao.verificaSeEstaOperante())
+                return true;
+        return false;
+    }
+}

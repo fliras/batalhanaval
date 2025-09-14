e309d0c003640b4c72d3496d9ecdff42ad87ac9a
Felipe Lira <filipi6219@gmail.com>
2025-09-14 18:27:31 -0300

    feat: add nome no enum de Tipos de embarcação

--- batalhanaval/src/main/java/br/edu/cefsa/batalhanaval/TiposDeEmbarcacao.java
+++ batalhanaval/src/main/java/br/edu/cefsa/batalhanaval/TiposDeEmbarcacao.java
@@ -1,19 +1,25 @@
 package br.edu.cefsa.batalhanaval;
 
 public enum TiposDeEmbarcacao {
-    PORTA_AVIOES(6),
-    CRUZADOR(5),
-    FRAGATA(4),
-    DESTROIER(3),
-    SUBMARINO(2);
+    PORTA_AVIOES("Porta Aviões", 6),
+    CRUZADOR("Cruzador", 5),
+    FRAGATA("Fragata", 4),
+    DESTROIER("Destróier", 3),
+    SUBMARINO("Submarino", 2);
     
     private int qtdeDePartes;
+    private String nome;
     
-    TiposDeEmbarcacao(int qtdeDePartes) {
+    TiposDeEmbarcacao(String nome, int qtdeDePartes) {
+        this.nome = nome;
         this.qtdeDePartes = qtdeDePartes;
     }
     
     public int obtemQtdeDePartes() {
         return qtdeDePartes;
+    }
+    
+    public String obtemNome() {
+        return nome;
     }
 }
